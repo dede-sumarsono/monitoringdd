9db0af6de5fdbb1984bbda0b821028fcda8e16f1
dede-sumarsono <dede.sumarsono45@gmail.com>
2023-07-03 23:34:23 +0700

    5. Layanan Screen

--- lib/screens/layanan_screen.dart
+++ lib/screens/layanan_screen.dart
@@ -17,6 +17,31 @@ class LayananScreen extends StatefulWidget {
 class _LayananScreenState extends State<LayananScreen> {
   //final userdata = Provider.of<Auth>(context);
 
+
+  List _listLayanan = [
+    ////peralihan Hak
+    'Jual Beli',
+    'Waris',
+    'Hibah',
+    'Tukar Menukar',
+    'Pemasukan ke dalam Perusahaan',
+    'Merger',
+    'Penetapan Pengadilan',
+    'Lelang',
+    'Pembagian Hak',
+    ////Hak Tanggungan
+    'Cessie',
+    'Subrogasi',
+    'Roya',
+    'Merger HT',
+    //layanan Notaris
+    'Akta PerjanjianKawin',
+    'Pendirian CV',
+    'Pendirian PT',
+    'Hibah Merk',
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     final userdata = Provider.of<Auth>(context);
@@ -87,36 +112,107 @@ class _LayananScreenState extends State<LayananScreen> {
               height: 20,
             ),
 
-            SizedBox(
-              height: 20,
-            ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Berkas yang anda ajukan',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                InkWell(
-                  onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));},
-                  child: Container(
-                    child: Text(
-                      'Lihat Semua >',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
+            Expanded(
+              child: ListView(
+                children: [
+                                                // Row(
+                                                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                //   children: [
+                                                //     Text(
+                                                //       'Peralihan Hak',
+                                                //       style: TextStyle(
+                                                //         color: Colors.white,
+                                                //         fontWeight: FontWeight.bold,
+                                                //         fontSize: 20,
+                                                //       ),
+                                                //     ),
+                                                //   ],
+                                                // ),
+                                                //
+                                                // SizedBox(height: 15,),
+                                                //
+                                                // SingleChildScrollView(
+                                                //   scrollDirection: Axis.horizontal,
+                                                //   child: Row(
+                                                //     //mainAxisAlignment: MainAxisAlignment.start,
+                                                //     children: [
+                                                //       for (int i = 1 ; i<5; i++)
+                                                //         Padding(
+                                                //           padding: EdgeInsets.only(left: 10),
+                                                //           child: ClipRRect(
+                                                //             borderRadius: BorderRadius.circular(15),
+                                                //             child: Image.asset(
+                                                //               "assets/img/jualbeli.png",
+                                                //               //"assets/img/jualbeli$i.png",
+                                                //               height: 120,
+                                                //               fit: BoxFit.cover,
+                                                //             ),
+                                                //           ),
+                                                //         )
+                                                //     ],
+                                                //   ),
+                                                // ),
+
+                                            //SizedBox(height: 40,),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Peralihan Hak',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
                       ),
-
-                    ),
+                    ],
                   ),
-                )],
-            ),
+
+                  SizedBox(height: 20,),
+                  HakTanggunganScroll(0,8,(){}),//untuk peralihan hak
+                  SizedBox(height: 40,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Hak Tanggungan',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 20,),
+                  HakTanggunganScroll(9,12,(){}),
+                  SizedBox(height: 40,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Layanan Notaris',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 20,),
+                  HakTanggunganScroll(13,16,(){}),
+
+
+                ],
+              ),
+            )
+
+
           ],
         ),
       )
@@ -135,4 +231,101 @@ class _LayananScreenState extends State<LayananScreen> {
 
     );
   }
+
+
+
+  HakTanggunganScroll(jumlah1,jumlah2,onClick) {
+    jumlah2++;
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          for(int i=jumlah1; i<jumlah2; i++)
+          InkWell(
+            onTap: (){},
+            child: Container(
+              width: 190,
+              height: 300,
+              margin: EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                //color: Color(0xFF292B37),
+                color: orangeDeep,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    //color: Color(0xFF292B37).withOpacity(0.5),
+                    color: Colors.orangeAccent.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 6
+                  )
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(10),
+                      topLeft: Radius.circular(10)
+                    ),
+                    child: Image.asset(
+                        'assets/img/ht2.jpg',
+                        //'assets/img/ht$i.jpg',
+                        height: 180,
+                        width: 190,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 5
+                      ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          //"Perjanjian Pra Nikah"
+                          "${_listLayanan[i]}"
+                        ,style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 19
+                          ),
+                        ),
+                        SizedBox(height: 5,),
+                        Text("  Hak Tanggungan",
+                          style: TextStyle(
+                            //color: Colors.white.withOpacity(0.7),
+                            color: Colors.white54,
+                            //fontSize: 16,
+                            fontWeight: FontWeight.w500),
+                          ),
+                        SizedBox(height: 8,),
+                        Row(
+                          children: [
+                            Icon(Icons.star,color: Colors.amber,),
+                            SizedBox(width: 5,),
+                            Text("8.5",
+                            style: TextStyle(
+                              color: Colors.white54,
+                              fontSize: 16
+                            ),)
+
+                          ],
+                        )
+
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+
 }
